cb2c2406d4f4921178d94460b8ca3b5338f13d7c
Vishnu Mouli <vishnumouli0@gmail.com>
2025-07-26 15:37:41 +0530

    auth final checkpoint with logout

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,9 @@
+import { Logout } from "@/components/logout";
+
 export default function Home() {
-	return <div className="text-red-500 text-5xl">Hello</div>;
+	return (
+		<div className="p-2">
+			<Logout />
+		</div>
+	);
 }
