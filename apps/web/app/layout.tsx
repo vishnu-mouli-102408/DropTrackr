e75cf7307cdcca4bdfc24c2460c8cd00f4573faa
Vishnu Mouli <vishnumouli0@gmail.com>
2025-07-26 14:03:05 +0530

    shadcn and ui changes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "@repo/ui/globals.css";
 import { Toaster } from "@repo/ui/components/sonner";
 import { Providers } from "../components/global/providers";
-
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
 	title: "Image Generator",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+			<body className={"antialiased dark min-h-screen overflow-x-hidden"}>
 				<main className="relative flex flex-col">
 					<Providers>{children}</Providers>
 					<Toaster closeButton position="bottom-center" theme="dark" richColors />
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-	return <div>check two</div>;
+	return <div className="text-red-500 text-5xl">Hello</div>;
 }
